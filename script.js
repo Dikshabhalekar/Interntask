84f86ca979b94033aa4193960f19fa23655a3de1
Dikshabhalekar <dikshabhalekar1@gmail.com>
2024-09-10 14:18:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script for handling dynamic behavior
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Thank you for your interest! More details will be coming soon.');
+});
